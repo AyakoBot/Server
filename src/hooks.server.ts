e1d7a9a0f0eb7ab95e01bb98c93397d8d92376fc
Larsundso <Larsundso3@gmail.com>
2024-05-15 21:14:47 +0200

    cdn handler

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,7 +1,13 @@
 import type { Handle } from '@sveltejs/kit';
+import { PUBLIC_CDN } from '$env/static/public';
+import cdn from '$lib/server/cdn';
 
 /** @type {import('@sveltejs/kit').Handle} */
 export const handle: Handle = async ({ event, resolve }) => {
+	if (event.url.href.startsWith(PUBLIC_CDN)) {
+		return cdn({ event, resolve });
+	}
+
 	event.url = new URL(event.url.href.replace('/api/', '/'));
 
 	return resolve(event);
