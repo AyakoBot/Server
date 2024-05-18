6063896b59af7347cacddaab1689daeab4a46ba8
Larsundso <Larsundso3@gmail.com>
2024-05-18 11:56:23 +0200

    add ratelimits

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,9 +1,28 @@
 import type { Handle } from '@sveltejs/kit';
 import { PUBLIC_CDN } from '$env/static/public';
 import cdn from '$lib/server/cdn';
+import { RetryAfterRateLimiter } from 'sveltekit-rate-limiter/server';
+
+const limiter = new RetryAfterRateLimiter({
+	IP: [10, '10s'],
+	IPUA: [10, '10s'],
+});
 
 /** @type {import('@sveltejs/kit').Handle} */
 export const handle: Handle = async ({ event, resolve }) => {
+	const status = await limiter.check(event);
+
+	if (status.limited) {
+		let response = new Response(
+			`You are being rate limited. Please try after ${status.retryAfter} seconds.`,
+			{
+				status: 429,
+				headers: { 'Retry-After': status.retryAfter.toString() },
+			},
+		);
+		return response;
+	}
+
 	if (event.url.href.startsWith(PUBLIC_CDN)) {
 		return cdn({ event, resolve });
 	}
