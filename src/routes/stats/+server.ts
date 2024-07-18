27d20312550765de0fca360d2629636af49bc8a9
Larsundso <Larsundso3@gmail.com>
2024-07-18 12:26:00 +0200

    stats compatibility

--- src/routes/stats/+server.ts
+++ src/routes/stats/+server.ts
@@ -3,12 +3,12 @@ import DataBase from '$lib/server/database.js';
 import type { RequestHandler } from './$types';
 
 export const GET: RequestHandler = async () => {
-	const count = await DataBase.stats.findFirst();
+	const count = await DataBase.stats.findFirst({ orderBy: { timestamp: 'desc' } });
 	if (!count) return json({ code: 424 });
 
 	return json({
-		guildCount: Number(count?.guildcount),
-		userCount: Number(count?.allusers),
+		guildCount: Number(count?.guildCount),
+		userCount: Number(count?.allUsers),
 	} as Returned);
 };
 
