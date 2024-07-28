284f10cc03dc3d306fa989322e5aaa595154d415
Larsundso <Larsundso3@gmail.com>
2024-07-28 17:33:15 +0200

    improve endpoint

--- src/routes/guilds/+server.ts
+++ src/routes/guilds/+server.ts
@@ -1,12 +1,57 @@
-import { json } from '@sveltejs/kit';
+import { error, json } from '@sveltejs/kit';
 import DataBase from '$lib/server/database.js';
 import type { RequestHandler } from './$types';
+import { GuildFeature } from 'discord-api-types/v10';
 
-export const GET: RequestHandler = async () => {
-	const guilds = await DataBase.guilds.findMany({ where: { membercount: { gte: 10000 } } });
-	if (!guilds) return json([]);
+export const GET: RequestHandler = async (req) => {
+	const take = parseInt(req.url.searchParams.get('take') ?? '100');
+	const skip = parseInt(req.url.searchParams.get('skip') ?? '0');
+	const leastMemberCount = parseInt(req.url.searchParams.get('leastMemberCount') ?? '10000');
 
-	return json(guilds.map((s) => ({ ...s, membercount: Number(s.membercount) })) as Returned);
+	if (isNaN(skip)) return error(400, 'skip is NaN');
+	if (isNaN(take)) return error(400, 'take is NaN');
+	if (isNaN(leastMemberCount)) return error(400, 'leastMemberCount is NaN');
+	if (take > 100) return error(400, 'Max take is 100');
+	if (take < 1) return error(400, 'Min take is 1');
+	if (skip < 0) return error(400, 'Min skip is 0');
+	if (leastMemberCount < 1) return error(400, 'Min leastMemberCount is 1');
+
+	const guilds = await DataBase.guilds.findMany({
+		where: {
+			membercount: { gte: leastMemberCount },
+			features: {
+				hasSome: [
+					GuildFeature.Discoverable,
+					GuildFeature.Community,
+					GuildFeature.Featurable,
+					GuildFeature.Partnered,
+					GuildFeature.VanityURL,
+				],
+			},
+		},
+		take,
+		skip,
+		orderBy: { name: 'desc' },
+		select: {
+			name: true,
+			banner: true,
+			guildid: true,
+			icon: true,
+			membercount: true,
+			invite: true,
+		},
+	});
+
+	return json(
+		guilds.map((s) => ({
+			guildid: s.guildid,
+			name: s.name,
+			icon: s.icon,
+			banner: s.banner,
+			invite: s.invite,
+			membercount: Number(s.membercount),
+		})) as Returned,
+	);
 };
 
 export type Returned = {
