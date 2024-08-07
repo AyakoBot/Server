51b81c1c4606ccf50b58e7ac1b96b9d1b3740002
Larsundso <Larsundso3@gmail.com>
2024-08-07 22:26:39 +0200

    remove old endpoints + ratelimits

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,22 +1,6 @@
 import { PUBLIC_CDN } from '$env/static/public';
-import sleep from '$lib/scripts/util/sleep';
 import cdn from '$lib/server/cdn';
 import { type Handle } from '@sveltejs/kit';
-
-const limiter = new Map<string, Set<number>>();
-
-setInterval(() => {
-	limiter.forEach((val, k) => {
-		if (val.size === 0) {
-			limiter.delete(k);
-			return;
-		}
-
-		val.forEach((v) => {
-			if (v < Date.now() - 10000) val.delete(v);
-		});
-	});
-}, 10000);
 
 /** @type {import('@sveltejs/kit').Handle} */
 export const handle: Handle = async ({ event, resolve }) => {
@@ -25,22 +9,6 @@ export const handle: Handle = async ({ event, resolve }) => {
 	}
 
 	event.url = new URL(event.url.href.replace('/api/', '/'));
-
-	const ip =
-		event.request.headers.get('X-forwarded-for') ??
-		event.request.headers.get('cf-connecting-ip') ??
-		crypto.randomUUID();
-	const now = Date.now();
-
-	if (Number(limiter.get(ip)?.size) >= 20) {
-		limiter.get(ip)!.add(now);
-
-		console.log(`Ratelimit for ${ip} exceeded`);
-		await sleep(2500 * Number(limiter.get(ip)?.size) - 20);
-	} else {
-		if (!limiter.has(ip)) limiter.set(ip, new Set());
-		limiter.get(ip)!.add(now);
-	}
 
 	return resolve(event);
 };
--- src/routes/qotd/+server.ts
+++
@@ -1,6 +0,0 @@
-import { error, json } from '@sveltejs/kit';
-import type { RequestHandler } from './$types';
-import qotd from '$lib/server/questions.json';
-
-export const GET: RequestHandler = async () =>
-	json({ question: qotd[new Date().getUTCDate() % qotd.length] });
