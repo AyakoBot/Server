52d563bf87c253918f50b40df563886e90ae1412
Larsundso <Larsundso3@gmail.com>
2024-09-10 12:57:23 +0200

    fix CDN

--- src/lib/server/cdn.ts
+++ src/lib/server/cdn.ts
@@ -1,16 +1,18 @@
 import type { Handle } from '@sveltejs/kit';
 import { error } from '@sveltejs/kit';
 import fs from 'fs';
-import { CDN } from '$env/static/private';
 import mime from 'mime';
 
 const handle: Handle = ({ event }) => {
-	const path = `${CDN}${event.url.pathname}`;
+	const path = `/app/Ayako/packages/CDN/${event.url.pathname}`;
 
 	try {
 		return new Response(fs.readFileSync(path), {
 			status: 200,
-			headers: [['Content-Type', mime.getType(path) || ''], ['Cache-Control', 'pulic, max-age=604800']],
+			headers: [
+				['Content-Type', mime.getType(path) || ''],
+				['Cache-Control', 'pulic, max-age=604800'],
+			],
 		});
 	} catch (e) {
 		console.log(e);
