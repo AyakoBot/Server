a72af9ebe44519558f174a5a42c04e44f1ca984d
Larsundso <Larsundso3@gmail.com>
2024-10-05 11:17:53 +0200

    check in-dev

--- src/lib/server/redis.ts
+++ src/lib/server/redis.ts
@@ -1,5 +1,6 @@
 import Redis from 'ioredis';
+import { dev } from '$env/static/private';
 
-const client = new Redis({ host: 'redis'});
+const client = new Redis({ host: dev ? '127.0.0.1' : 'redis' });
 
 export default client;
