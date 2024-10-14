3c40f56fb471cfdc6acf9b6781fdc69d44ac4748
Larsundso <Larsundso3@gmail.com>
2024-10-14 12:31:39 +0200

    rm redundant

--- src/lib/server/cdn.ts
+++ src/lib/server/cdn.ts
@@ -12,12 +12,7 @@ const handle: Handle = ({ event }) => {
 			headers: [
 				['Content-Type', mime.getType(path) || ''],
 				['Cache-Control', `pulic, max-age=${path.includes('antivirus') ? 1 : 604800}`],
-				['Content-Security-Policy', "default-src 'self'; frame-ancestors 'none';"],
 				['Strict-Transport-Security', 'max-age=31536000; includeSubDomains; preload'],
-				['X-Frame-Options', 'DENY'],
-				['X-Content-Type-Options', 'nosniff'],
-				['Referrer-Policy', 'strict-origin-when-cross-origin'],
-				['Permissions-Policy', 'camera=(), microphone=(), document-domain=()'],
 			],
 		});
 	} catch (e) {
