d546ebd2618cbcdee12a2b1e355fe70e88333308
Larsundso <Larsundso3@gmail.com>
2025-01-26 22:50:39 +0100

    new table scheme

--- src/lib/scripts/util/getPunishments.ts
+++ src/lib/scripts/util/getPunishments.ts
@@ -1,5 +1,4 @@
 import DataBase from '$lib/server/database.js';
-import { PunishmentType } from '@ayako/website/src/lib/scripts/types';
 
 /**
  * Retrieves a list of punishments for a specific user in a guild.
@@ -28,40 +27,5 @@ export default async ({
 		orderBy: { uniquetimestamp: 'desc' } as const,
 	};
 
-	const results = await DataBase.$transaction([
-		DataBase.punish_bans.findMany(where),
-		DataBase.punish_channelbans.findMany(where),
-		DataBase.punish_kicks.findMany(where),
-		DataBase.punish_mutes.findMany(where),
-		DataBase.punish_warns.findMany(where),
-		DataBase.punish_tempchannelbans.findMany(where),
-		DataBase.punish_tempbans.findMany(where),
-		DataBase.punish_tempmutes.findMany(where),
-	]);
-
-	return results
-		.filter((p) => !!p.length)
-		.map((r, i) => {
-			switch (i) {
-				case 0:
-					return r.map((p) => ({ ...p, type: PunishmentType.bans }));
-				case 1:
-					return r.map((p) => ({ ...p, type: PunishmentType.channelbans }));
-				case 2:
-					return r.map((p) => ({ ...p, type: PunishmentType.kicks }));
-				case 3:
-					return r.map((p) => ({ ...p, type: PunishmentType.mutes }));
-				case 4:
-					return r.map((p) => ({ ...p, type: PunishmentType.warns }));
-				case 5:
-					return r.map((p) => ({ ...p, type: PunishmentType.tempchannelbans }));
-				case 6:
-					return r.map((p) => ({ ...p, type: PunishmentType.tempbans }));
-				case 7:
-					return r.map((p) => ({ ...p, type: PunishmentType.tempmutes }));
-				default:
-					return [];
-			}
-		})
-		.flat();
+	return DataBase.punishments.findMany(where);
 };
