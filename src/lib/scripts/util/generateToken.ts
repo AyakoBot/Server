449bb847710013b465e772fbfdc93d5882f7f0c8
Larsundso <Larsundso3@gmail.com>
2025-04-15 17:34:54 +0200

    remove bcrypt

--- src/lib/scripts/util/generateToken.ts
+++ src/lib/scripts/util/generateToken.ts
@@ -1,22 +1,25 @@
-import bcrypt from 'bcrypt';
 import crypto from 'crypto';
-import getRandom from './getRandom.js';
 
 /**
  * Generates a URL-safe token for a given user ID.
  *
- * This function creates a random UUID, hashes it using bcrypt, encodes the user ID in base64,
- * and combines these elements into a URL-safe base64 string.
+ * This function creates a random token using Node.js crypto module,
+ * encodes the user ID in base64, and combines these elements 
+ * into a URL-safe base64 string.
  *
  * @param userId - The user ID to be included in the token.
  * @returns A promise that resolves to a URL-safe base64 encoded token.
  */
-export default async (userId: string) =>
-	`${Buffer.from(userId).toString('base64')}.${(
-		await bcrypt.hash(crypto.randomUUID(), getRandom(5, 10))
-	)
-		.split(/\$/g)
-		.at(-1)}`
-		.replace(/\+/g, '-')
-		.replace(/\//g, '_')
-		.replace(/=+$/, '');
+export default async (userId: string) => {
+  const randomBytes = crypto.randomBytes(32);
+  const hash = crypto
+    .createHash('sha256')
+    .update(randomBytes)
+    .update(crypto.randomUUID())
+    .digest('base64');
+  
+  return `${Buffer.from(userId).toString('base64')}.${hash}`
+    .replace(/\+/g, '-')
+    .replace(/\//g, '_')
+    .replace(/=+$/, '');
+};
