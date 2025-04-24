54b66ab5b59a08cf2b89e2b4a09fdf5a6701f9fb
Larsundso <Larsundso3@gmail.com>
2025-04-24 09:50:26 +0200

    proper building

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,15 @@
 // @ts-expect-error
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import { resolve } from 'node:path';
 
 export default defineConfig({
 	server: { allowedHosts: ['api.animekos.org'] },
 	plugins: [sveltekit()],
+	resolve: {
+		alias: {
+			'@ayako/bot': resolve(process.cwd(), '../Bot'),
+			'@ayako/website': resolve(process.cwd(), '../../apps/Website')
+		}
+	}
 });
