73379b979d4f5107a0405b98ea9b7138d4f6c825
Larsundso <Larsundso3@gmail.com>
2025-06-19 11:32:34 +0200

    related https://github.com/AyakoBot/Ayako-v2/issues/360

--- src/routes/v1/guilds/[guildId]/linked-roles/[settingsId]/decoration/+server.ts
+++ src/routes/v1/guilds/[guildId]/linked-roles/[settingsId]/decoration/+server.ts
@@ -27,10 +27,10 @@ export const GET: RequestHandler = async (req) => {
 	const settings = await DataBase.linkedRolesDeco.findFirst({
 		where: { guildid: guildId.data, uniquetimestamp: settingsId.data },
 	});
-	if (!settings) error(404, 'Settings not found');
-	if (!settings.botId) error(424, 'Bot ID missing in Settings');
-	if (!settings.botSecret) error(424, 'Bot Secret missing in Settings');
-	if (!settings.botToken) error(424, 'Bot Token missing in Settings');
+	if (!settings) return error(404, 'Settings not found');
+	if (!settings.botId) return error(424, 'Bot ID missing in Settings');
+	if (!settings.botSecret) return error(424, 'Bot Secret missing in Settings');
+	if (!settings.botToken) return error(424, 'Bot Token missing in Settings');
 	if (!code)
 		throw redirect(
 			302,
@@ -55,12 +55,18 @@ export const GET: RequestHandler = async (req) => {
 
 	const userAPI = APIManager.makeAPI(tokens.access_token);
 	const identity = await userAPI.oauth2.getCurrentAuthorizationInformation();
-	if (!identity.user) error(401, 'User not found. Scope "identify" missing');
-	if (!identity.scopes.includes(OAuth2Scopes.Identify)) error(401, 'Scope "identify" missing');
+	if (!identity.user) return error(401, 'User not found. Scope "identify" missing');
+	if (!identity.scopes.includes(OAuth2Scopes.Identify))
+		return error(401, 'Scope "identify" missing');
 	if (!identity.scopes.includes(OAuth2Scopes.RoleConnectionsWrite)) {
-		error(401, 'Scope "role_connections.write" missing');
+		return error(401, 'Scope "role_connections.write" missing');
 	}
-	if (!settings.allowedUsers.includes(identity.user.id)) error(403, 'User not allowed');
+	if (
+		!settings.allowedUsers.includes(identity.user.id) &&
+		!(await allowedByRoles(botAPI, identity.user.id, settings))
+	) {
+		return error(403, 'User not allowed');
+	}
 
 	await DataBase.linkedRoleTokens.create({
 		data: {
@@ -78,8 +84,7 @@ export const GET: RequestHandler = async (req) => {
 		})
 		.catch((e: DiscordAPIError) => e);
 
-
-	if ('message' in res) error(500, `Failed to update role connection\n${res.message}`);
+	if ('message' in res) return error(500, `Failed to update role connection\n${res.message}`);
 
 	return json({
 		success: true,
@@ -91,3 +96,12 @@ const getRedirectURI = (settings: linkedRolesDeco) =>
 	`https://api.ayakobot.com/v1/guilds/${settings.guildid}/linked-roles/${settings.uniquetimestamp}/decoration`;
 
 export type GETResponse = { success: boolean; message: string };
+
+const allowedByRoles = async (api: API, userId: string, settings: linkedRolesDeco) => {
+	if (!settings.allowedRoles.length) return false;
+
+	const member = await api.guilds.getMember(settings.guildid, userId).catch(() => null);
+	if (!member) return false;
+
+	return member.roles.some((role) => settings.allowedRoles.includes(role));
+};
